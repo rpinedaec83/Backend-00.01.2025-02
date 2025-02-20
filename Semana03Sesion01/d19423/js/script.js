4606deef9dad849c9719b2819a26d341487b09a3
Said <saialf1911@gmail.com>
2025-02-19 19:32:42 -0500

    Ultima modificacion

--- Semana03Sesion01/d19423/js/script.js
+++ Semana03Sesion01/d19423/js/script.js
@@ -1,1 +1,11 @@
 console.log("Hola desde script.js");
+document.getElementById("Titulo").innerHTML = "Hola desde el titulo"
+
+let nombre= prompt("Ingerese su nombre");
+
+document.getElementById("titulo").innerHTML = "Hola" + nombre
+document.getElementById("btnOtro").addEventListener("click", function()){
+    let apellido = document.getElementById("apellido").value;
+    document
+
+}
