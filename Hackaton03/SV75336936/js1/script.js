bd86e7daf0b8df60391da6caac2d697f69bfc245
sandro <sandrogt05@gmail.com>
2025-02-22 15:37:10 -0500

    listo1

--- Hackaton03/SV75336936/js1/script.js
+++ Hackaton03/SV75336936/js1/script.js
@@ -9,3 +9,17 @@ function ejercicio01(){
         alert("No tiene 3 digitos")
     }
 }
+
+function ejercicio02(){
+
+
+    let num = parseInt(numero);
+    
+    if (isNaN(num)) {
+        console.log('Por favor, ingrese un número válido.');
+    } else if (num < 0) {
+        console.log('El número es negativo.');
+    } else {
+        console.log('El número no es negativo.');
+    }
+}
