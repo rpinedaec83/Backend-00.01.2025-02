dd5ca20a61afd17ffb5b38a84471d5230fe6d3ec
Said <saialf1911@gmail.com>
2025-02-22 17:20:07 -0500

    Ejercicio 30

--- Hackaton03/SV71542188/script.js
+++ Hackaton03/SV71542188/script.js
@@ -465,3 +465,11 @@ function ejercicio29() {
     }
     alert("La suma de los primeros cien números es: " + suma);
 }
+
+function ejercicio30() {
+    let suma = 0;
+    for (let i = 1; i <= 100; i++) {
+        suma += i;
+    }
+    alert("La suma de los primeros cien números es: " + suma);
+}
