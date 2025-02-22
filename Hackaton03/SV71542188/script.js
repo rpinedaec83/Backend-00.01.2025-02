28a8b0694cc920fa74af8da92555cce7e1e2b589
Said <saialf1911@gmail.com>
2025-02-22 16:43:50 -0500

    Ejercicio 16

--- Hackaton03/SV71542188/script.js
+++ Hackaton03/SV71542188/script.js
@@ -217,3 +217,33 @@ function ejercicio15(){
     }
 }
 
+function ejercicio16(){
+    let numero = parseInt(prompt("Ingrese un número del 1 al 7:"));
+    let dia;
+    switch (numero) {
+        case 1:
+            dia = "Lunes";
+            break;
+        case 2:
+            dia = "Martes";
+            break;
+        case 3:
+            dia = "Miércoles";
+            break;
+        case 4:
+            dia = "Jueves";
+            break;
+        case 5:
+            dia = "Viernes";
+            break;
+        case 6:
+            dia = "Sábado";
+            break;
+        case 7:
+            dia = "Domingo";
+            break;
+        default:
+            dia = "Número no válido. Por favor ingrese un número del 1 al 7.";
+    }
+    alert(dia);
+}
