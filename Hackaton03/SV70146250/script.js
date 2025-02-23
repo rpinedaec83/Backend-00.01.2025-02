ad31bbb3ae676893ab95d634e76a4501e8c75f62
User <yo@3232.32>
2025-02-22 19:07:57 -0500

    Avance#2:index_fonts-script_E1

--- Hackaton03/SV70146250/script.js
+++ Hackaton03/SV70146250/script.js
@@ -10,7 +10,24 @@ function ejercicio01(){
     else{
         alert("No tiene 3 digitos")
     }
-    
 */
+let num = prompt("Ingrese un numero:"); // Pedir numero al usuario
+num = parseFloat(num); // Convertir a número decimal
+if (!num) { //Evalua que el dato ingresado sea Float, igual que el num.
+    alert("Error: Debe ser un numero");
+}else{
+    // Evaluar si no es entero, Math.trunc elimina el decimal
+    if (Math.trunc(num) !== num) { //!== Diferencia estricta
+        alert("OJO: Solo se evalua la parte entera");
+    }
+    // Extraer parte entera y hacer positivo
+    num = Math.trunc(Math.abs(num)); 
 
+    // Evaluar si tiene 3 digitos
+    if (num >= 100 && num <= 999) {
+        alert("[Tiene 3 Digitos]");
+    } else {
+        alert("[No tiene 3 Digitos]");
+    }
+}
 }
