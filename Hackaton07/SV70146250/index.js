4234cf9e47772995c8411852c6286351135a9532
Kevin Cossio M <198007979+KevinCossioM@users.noreply.github.com>
2025-03-23 02:24:57 -0500

    Avance#4:PokeList_PokePoder_PrincipalesRick&Morty_Rick&MortyPErsonajes_Default-Leyenda

--- Hackaton07/SV70146250/index.js
+++ Hackaton07/SV70146250/index.js
@@ -15,6 +15,7 @@ http
     
         let htmlResponse = "";
         switch (tipo) {
+//#region Git User
           case "/gituser":    //Primer API
           let usuario = Object.keys(query)[0]; 
           if (!usuario) {
@@ -51,6 +52,7 @@ http
           }
           res.end();
           break;
+//#region Clima
           case "/clima":
             let ciudad = Object.keys(query)[0]; 
             console.log(ciudad);
@@ -75,7 +77,7 @@ http
             res.write(`El clima en <b> ${ciudad} es ${temp} grados Celsius </b>`);
             res.end();
             break;
-
+//#region Tipo cambio
              case "/tipocambio":
                     let moneda = query.moneda; 
                     try {
@@ -96,11 +98,154 @@ http
                   res.write(htmlResponse);
                   res.end();
                   break;
+//#region PokeLista
+            /*
+              case '/pokemon':
+                let pokelista ={
+                    method: 'get',
+                    url: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1302',
+                    headers:{}
+                }
+                axios.request(pokelista)
+                .then((response) => {
+                    console.log(JSON.stringify(response.data));
+                    res.write(JSON.stringify(response.data))
+                    return res.end();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+              break;  
+            */
+//#endregion
+              case "/pokemonlist": // API de Pokémon
+              try {
+                let response = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1302");
+                let poketomonster = response.data.results;
+                
+                res.write("<h1>Lista Pokemon</h1><ul>");
+                poketomonster.forEach((pokemon,num) => {
+                  res.write(`<p>${num + 1}. ${pokemon.name}</p>`);
+                });
+                res.write("</ul>");
+              } catch (error) {
+                res.write("Error Datos Invalidos");
+              }
+              res.end();
+              break;
+//#region PokePoderes
+            case "/pokemon":
+              let poketmonster = Object.keys(query)[0]; // Captura el nombre del Pokémon
+              if (!poketmonster) {
+                res.write("Pokemon NO existe");
+                res.end();
+                return;
+              }
+              try {
+                let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${poketmonster}`);
+                let data = response.data;
+                let nombrePokemon = data.forms[0].name; 
+                let movimientos = data.moves.map(mov => mov.move.name).join(", "); //Movimientos
 
-    
+                let htmlResponse = `
+                  <h2>${nombrePokemon}</h2>
+                  <p>Poderes: ${movimientos}</p>
+                `;
+                res.write(htmlResponse);
+              } catch (error) {
+                res.write("Error: Datos Invalidos");
+              }
+              res.end();
+              break;
+//#region Rick&Morty Personajes
+            case "/rick&mortypersonajes":
+              let id = Object.keys(query)[0]; // Obtiene el ID del personaje
+
+              if (!id) {
+                res.write("<p>Dato Invalido</p>");
+                res.end();
+                break;
+              }
+
+              try {
+                let response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+                let data = response.data;
+
+                let htmlResponse = `
+                  <h1>${id}: ${data.name}</h1>
+                  <p>Estado: ${data.status}</p>
+                  <p>Especie: ${data.species}</p>
+                  <p>Origen: ${data.origin.name}</p>
+                  <p>Ubicacion: ${data.location.name}</p>
+                  <img src="${data.image}" width="150">
+                `;
+
+                res.write(htmlResponse);
+              } catch (error) {
+                res.write("Error: Solo existen 826 personajes");
+              }
+
+              res.end();
+              break;
+//#region Principales Rick&Morty
+              case "/principalesrick&morty":
+                  try {
+                  let htmlResponse = "<h1>Principales de Rick & Morty</h1><ul>";
+                  
+                  for (let i = 1; i <= 20; i++) {
+                    let response = await axios.get(`https://rickandmortyapi.com/api/character/${i}`);
+                    let data = response.data;
+                    htmlResponse += `<p>${i}: ${data.name}</p>`;
+                  }
+
+                  htmlResponse += "</ul>";
+                  res.write(htmlResponse);
+                } catch (error) {
+                  res.write("Error: Datos Ivalidos");
+                }
+
+                res.end();
+                break;
+
+//#region Leyenda
           default:          //De no encontrarse la ruta
-            res.write(`<div style='display:grid;place-items:center;height:100vh;text-align:center;font-size:4rem;'>
-                        <p>Error 404<br>Ruta no encontrada</p></div>`);
+            res.write(`<h1>Leyenda</h1>
+                        <table border="1">
+              <tr>
+                  <th>API</th>
+                  <th>Ejemplo</th>
+              </tr>
+              <tr>
+                  <td>Git User</td>
+                  <td>/gituser?rpinedaec83</td>
+              </tr>
+              <tr>
+                  <td>Clima</td>
+                  <td>/clima?Lima</td>
+              </tr>
+              <tr>
+                  <td>Tipo Cambio</td>
+                  <td>/tipocambio?moneda=USD</td>
+              </tr>
+              <tr>
+                  <td>Lista Pokemon</td>
+                  <td>/pokemonlist</td>
+              </tr>
+              <tr>
+                  <td>Poderes Pokemon</td>
+                  <td>/pokemon?ditto</td>
+              </tr>
+              <tr>
+                  <td>Principales Rick&Morty</td>
+                  <td>/principalesrick&morty</td>
+              </tr>
+              <tr>
+                  <td>Rick&Morty Personaje</td>
+                  <td>/rick&mortypersonajes?2</td>
+              </tr>
+              
+              
+          </table>`);
             res.end();
           break;
 
