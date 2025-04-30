2f685bf765c8f3da35c3ee125a8e0c0e229fc578
unknown <angelamarcelop@gmail.com>
2025-04-30 11:22:11 -0500

    hackaton12@

--- Hackaton12/SV74579446/backend/index.js
+++ Hackaton12/SV74579446/backend/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
