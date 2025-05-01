69f3625bd14a07ed583cf713ee99bc55dbce0b7a
Roberto Pineda <rpineda@x-codec.net>
2025-04-30 22:35:21 -0500

    Hackaton 13

--- Semana13Sesion01/d19423/server.js
+++ Semana13Sesion01/d19423/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cookieSession = require('cookie-session');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
 
 
 const app = express();
@@ -10,8 +13,19 @@ const PORT = process.env.PORT || 8080;
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const storage = multer.diskStorage({
+    destination(req,res,cb){
+        cb(null,'storage')
+    },
+    filename(req,file,cb){
+        cb(null,file.originalname)
+    }
+});
+const upload = multer({storage});
+
 
 app.use(
     cookieSession({
@@ -23,6 +37,16 @@ app.use(
 
 app.get('/',(req,res)=>{
     res.send({message:"Bienvenido a mi API"})
+})
+
+app.post('/upload', upload.single('image'), (req,res)=>{
+    try {
+        if(req.file){
+            res.json(req.file);
+        }
+    } catch (error) {
+        res.status(500).send({message:error});
+    }
 })
 
 require('./app/routes/auth.routes')(app);
