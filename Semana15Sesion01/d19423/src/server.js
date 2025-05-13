5b3135f5a2930191044b27ad3488f038ea9f6e08
Roberto David Pineda Lopez <rpineda@MacBook-Pro-de-Roberto.local>
2025-05-12 22:30:50 -0500

    Clase 12052025 segunda parte

--- Semana15Sesion01/d19423/src/server.js
+++ Semana15Sesion01/d19423/src/server.js
@@ -10,6 +10,13 @@ const session = require('express-session');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+const SECRET = process.env.SECRET;
+
+const CON = require('./database/db');
+
 
 app.use(
     cookieSession({
@@ -18,17 +25,22 @@ app.use(
     })
 );
 
+require('./passport');
+
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({secret:'SECRETO'}));
+app.use(session({secret:SECRET}));
 
 app.get('/',(req,res)=>{
     res.status(200).send("Bienvenido a mi server");
 })
 
+app.get('/google',passport.authenticate("google", {
+    scope:["email"]
+}))
 
 
-server.listen(7979, ()=>{
-    console.log("Inicio el servidor")
+server.listen(PORT, ()=>{
+    console.log("Inicio el servidor en el puerto "+PORT)
 })
