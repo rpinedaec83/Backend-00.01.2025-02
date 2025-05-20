44cdf5da37d0f2be8a4c0615116f6fe2e78e29f4
Roberto David Pineda Lopez <rpineda@MacBook-Pro-de-Roberto.local>
2025-05-19 21:20:13 -0500

    Clase 19052025 segunda parte

---
+++ Semana16Sesion01/d19423/front/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Cancel from "./componets/Cancel";
+import Success from "./componets/Success";
+
+function App(){
+    return(
+        <BrowserRouter>
+         <Routes>
+            <Route path="/cancel" element={<Cancel/>}></Route>
+            <Route path="/success" element={<Success/>}></Route>
+         </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
